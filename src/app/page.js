7df1a25fbcd20b33ccb825952bf49ec54b411156
Heyson <heysonbr@outlook.es>
2024-09-20 13:29:55 +0200

    Actualizar título y descripción en layout.js y página principal. FALTA RESPONSIVE

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Bingo from "@/components/bingo";
 
 export default function Home() {
   return (
-    <div className="container flex justify-center items-center ">
+    <div className="">
       <Bingo />
     </div>
   );
